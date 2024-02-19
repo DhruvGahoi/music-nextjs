4a06986cafcdc3cdcfbff360c7f470506a1916b1
DhruvGahoi <dhruvgahoi11111@gmail.com>
2024-02-19 11:38:50 +0530

    added the hero section and some UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0..96] antialiased bg-grid-white/[0.02]">
-      <h1 className="text-3xl text-center"></h1>
+      <h1 className="text-3xl text-center">
+          <HeroSection />
+          <FeaturedCourses />
+          <Footer />
+      </h1>
     </main>
   );
 }
